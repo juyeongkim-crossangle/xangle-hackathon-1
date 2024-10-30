562ba3904bb4a39b073b1b634aa0ac3ec73da504
JuyeongKim <juyeongkim@crossangle.io>
2024-10-30 19:08:42 +0900

    feat: shadcn 설치 및 기본 컴포넌트 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+import {Button} from "@/components/ui/button";
+import {InputCard} from "@/components/test/InputCard";
+
 export default function Home() {
   return (
+      <div>
         <h1>Home Page</h1>
+        <div className="flex flex-col p-4 gap-2">
+            <div>
+              <Button
+                  variant="outline"
+                  size="default"
+              >
+                  Shadcn 버튼
+              </Button>
+            </div>
+            <div>
+                <InputCard />
+            </div>
+        </div>
+      </div>
   );
 }
